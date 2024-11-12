0fe4d885f587c343d7a94e09b274ac18f37ac82d
Ismayil <mohsumovismayil@gmail.com>
2024-11-12 12:41:45 +0100

    Create title component

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
-import {getFontFamily} from './assets/fonts/helper';
+import Title from './components/title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '100')}}>
-        Hello World
-      </Text>
+      <Title title={'Lets Explore'} />
     </SafeAreaView>
   );
 };
